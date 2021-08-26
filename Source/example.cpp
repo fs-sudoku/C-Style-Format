01cf86108bc845cb989e9e72a3ffbc0a0dcbbadd
Sudoku <85125559+fs-sudoku@users.noreply.github.com>
2021-08-26 22:21:57 +0300

    Update example.cpp

--- Source/example.cpp
+++ Source/example.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 
-#include "ñ-formating.h"
+#include "Ã±-formating.h"
 
 int main(int argc, char* argv[])
 {
 	const std::string format = "The integer value: %d, the string value: %s";
-	const std::string string = "Pushed string";
 
 	/* static format example */
 	const std::string static_message = StringFormatStatic<128>(format, 10, "Static example");
@@ -13,7 +12,7 @@ int main(int argc, char* argv[])
 	std::cout << static_message << std::endl;
 
 	/* dynamic format example */
-	const std::string dynamic_message = StringFormatDynamic(format, 15, string);
+	const std::string dynamic_message = StringFormatDynamic(format, 15, "Dynamic example");
 
 	std::cout << dynamic_message << std::endl;
 
